a12ca8587dbed82657314a5b5e0eac22b49fdc39
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-05-16 21:16:14 +0530

    Operators in JS

---
+++ Web_Dev_2026/Javascript/lect41/index.js
@@ -0,0 +1,152 @@
+//Operators: Related to operations. There are some specific symbols which performs a specific operation and carries a meaning as well, these symbols are known as operators.
+
+
+//1. Arithmetic Operators:
+
+
+//Binary Operators:
+let a = 10;
+let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// a = 2;
+// console.log(a ** b);
+
+//Unary Operators:
+
+// console.log(a++);
+// console.log(a--);
+// console.log(--a);
+// console.log(++a);
+
+
+//2. Assignment Operators:
+
+// let age = 20;
+// console.log(age);
+
+//shorthand notations to represent assignment operators:
+// age += 2;
+// console.log(age);
+// age -= 2;
+// console.log(age);
+// age /= 2;
+// console.log(age);
+// age *= 3;
+// console.log(age);
+
+
+//3. Comparison Operators:
+
+let num1 = 20;
+let num2 = 10;
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2);
+// console.log(num1 != num2);
+// console.log('20' === '20');
+// console.log(20 !== '20');
+
+//4. Ternary Operators:
+
+// let age = 2;
+// let status1 = (age > 18) ? "I can vote" : "I cannot vote";
+// console.log(status1);
+
+//5. Logical Operators:
+
+//And operator:
+let ans = true && false;
+// ans = false && false;
+// ans = true && true;
+// console.log(ans);
+
+//OR operator:
+
+let ans2 = true || false;
+// ans2 = false || false;
+// ans2 = true || true;
+// console.log(ans2);
+
+//NOT operator:
+
+// console.log(!true);
+// console.log(!false);
+
+//Working with non-booleans:
+
+//Falsy values -> undefined, null, 0, false, NaN, ''
+
+// console.log(true && NaN);
+// console.log(false && 'Aanchal');
+// console.log(true && 'Aanchal');
+
+// console.log(false || 'Aanchal');
+// console.log(false || '');
+// console.log(false || 0);
+
+//Short-circuiting:
+
+// console.log(false || 7 || 8 || 9 || false);
+
+//6. Bitwise Operators:
+
+//Bitwise AND:
+console.log(2 & 5);
+
+//Bitwise OR:
+console.log(2 | 5);
+
+//Bitwise NOT:
+console.log(~0);
+
+//Bitwise XOR: 
+console.log(2 ^ 2);
+console.log(2 ^ 3);
+
+//Right Shift Operator 
+
+console.log(10 >> 1);
+console.log(10 >> 2);
+
+//Left Shift Operator:
+
+console.log(5 << 1);
+console.log(5 << 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
