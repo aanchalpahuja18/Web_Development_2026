3bf7146f4d3168abcd0be9927bcad61af52dd9ee
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-05-18 20:14:34 +0530

    Loops in JS

---
+++ Web_Dev_2026/Javascript/lect42/index.js
@@ -0,0 +1,90 @@
+//Loops and Strings in JS
+
+//1. for loop:
+
+// for(let i=1;i<=10;i++){
+//     console.log("Aanchal");
+    
+// }
+
+// Counting in for loops
+
+// for(let i = 1;i<=5; i++){
+//     console.log(i);
+// }
+
+//Reverse counting in for loops:
+
+// for(let i=5; i>0;i--){
+//     console.log(i);
+    
+// }
+
+//break keyword in for loop:
+
+// for(let i=1;i<=5;i++){
+//     if(i==4)
+//         break;
+//     else
+//         console.log(i);
+        
+// }
+
+//continue keyword in JS:
+
+// for(let i=1;i<=5;i++){
+//     if(i==3)
+//         continue;
+//     else
+//         console.log(i);
+        
+// }
+
+
+//2. while loop in JS:
+
+// let i = 1;
+// while(i <= 10){
+//     console.log("Aanchal");
+//     i++;
+// }
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;    
+// }
+
+// let i = 5;
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+//while with break keyword:
+
+// let i = 1;
+// while(i <= 5){
+//     if(i==3){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//while with continue keyword:
+
+// let i = 1;
+// while(i <= 5){
+//     console.log("Hey");
+//     if(i==3){
+//         i++;
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+//     i++;
+// }
