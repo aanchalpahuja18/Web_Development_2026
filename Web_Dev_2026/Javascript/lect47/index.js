f5955aac2eca8c8afbaf379d73e1bbd5bab22385
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-05-26 18:26:07 +0530

    Practiced Classes again

--- Web_Dev_2026/Javascript/lect47/index.js
+++ Web_Dev_2026/Javascript/lect47/index.js
@@ -1,45 +1,113 @@
-// Classes and Default Parameters in JS:
+// // Classes and Default Parameters in JS:
 
-//1. Classes:
+// //1. Classes:
 
-class Human {
-    age;
-    #wt;
-    ht;
+// class Human {
+//     age;
+//     #wt;
+//     ht;
 
-    constructor(newAge, newHt, newWt){
+//     constructor(newAge, newHt, newWt){
+//         this.age = newAge;
+//         this.newHt = newHt;
+//         this.#wt = newWt;
+//     }
+
+//     walking(){
+//         console.log("I am walking", this.#wt);
+//     }
+//     running(){
+//         console.log("I can run");
+//     }
+
+//     //getters and setters:
+
+//     get fetchWeight(){
+//         return this.#wt;
+//     }
+//     set updateHeight(val){
+//         this.ht = val;
+//     }
+// }
+
+// let obj = new Human();
+// console.log(obj.age);
+// // console.log(obj.#wt);
+
+// obj.ht = 190;
+// console.log(obj.ht);
+
+// obj.walking()
+
+// obj = new Human(22, 190, 82);
+
+// console.log(obj.age);
+
+
+
+//Classes Practice:
+
+class Human{
+    age = 22;
+    #wt = 80;
+    ht = 180; 
+
+    constructor(newAge, newWt, newHt){
         this.age = newAge;
-        this.newHt = newHt;
-        this.#wt = newWt;
+        this.wt = newWt;
+        this.ht = newHt;
     }
-
+    
     walking(){
-        console.log("I am walking", this.#wt);
-    }
-    running(){
-        console.log("I can run");
+        console.log("I can walk!", this.#wt);
     }
 
-    //getters and setters:
+    running(){
+        console.log("I can run!", this.ht, "mtrs");
+    }
 
     get fetchWeight(){
         return this.#wt;
     }
-    set updateHeight(val){
-        this.ht = val;
+
+    set updateWeight(value){
+        this.#wt = value
     }
 }
 
-let obj = new Human();
-console.log(obj.age);
+
+//creating an object/instantiating the class:
+
+let obj = new Human;
+
+//Accessing the data members and functions of the class:
+// console.log(obj.age);
+
+// obj.walking();
+
+// console.log(obj.walking);
+
+//New object using constructor:
+
+// let obj2 = new Human(25, 70, 190);
+// console.log(obj2);
+
+// console.log(obj2.ht);
+
+// obj2.running()
+
+
+//Accessing private members
 // console.log(obj.#wt);
 
-obj.ht = 190;
-console.log(obj.ht);
+// obj.walking();
 
-obj.walking()
+//Using getters and setters:
 
-obj = new Human(22, 190, 82);
+// console.log(obj.fetchWeight);
 
-console.log(obj.age);
-console.log(obj.ht);
+// obj.updateWeight = 90;
+// console.log(obj.fetchWeight);
+
+
+
