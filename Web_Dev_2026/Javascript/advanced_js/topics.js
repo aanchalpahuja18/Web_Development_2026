cbb7c591812c092adb80cc5df97bf07ae34aa433
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-06-29 20:25:30 +0530

    Optional Chaining and Modules in JS

---
+++ Web_Dev_2026/Javascript/advanced_js/topics.js
@@ -0,0 +1,17 @@
+// Optional Chaining: Check if this exists. If yes continue. If not stop safely.”
+
+let user = {
+    name : "Aanchal"
+}
+
+console.log(user.name);
+
+console.log(user.profile?.age);
+
+//Modules in JS: In bigger projects we don’t keep everything in one file.
+
+//export: means I want other files to use this
+
+//import -> Means: bring code from another file
+
+//this keyword: this refers to the object calling the function.
