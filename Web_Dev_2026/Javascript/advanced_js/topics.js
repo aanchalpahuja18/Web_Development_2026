fcac1a2823255f8029fce3c31241966ee8ab103e
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-06-29 20:36:41 +0530

    this and Mutability in JS

--- Web_Dev_2026/Javascript/advanced_js/topics.js
+++ Web_Dev_2026/Javascript/advanced_js/topics.js
@@ -4,9 +4,9 @@ let user = {
     name : "Aanchal"
 }
 
-console.log(user.name);
+// console.log(user.name);
 
-console.log(user.profile?.age);
+// console.log(user.profile?.age);
 
 //Modules in JS: In bigger projects we don’t keep everything in one file.
 
@@ -15,3 +15,45 @@ console.log(user.profile?.age);
 //import -> Means: bring code from another file
 
 //this keyword: this refers to the object calling the function.
+
+let user2 = {
+   name: "Aanchal",
+
+   greet: function(){
+      console.log(this.name);
+   }
+}
+
+user2.greet();
+
+//Arrow functions do not create their own this
+
+let user3 = {
+   name: "Aanchal",
+
+   greet: () => {
+      console.log(this.name)
+   }
+}
+
+user3.greet(); //output will be undefined
+//Because arrow function borrows surrounding this
+
+//Mutability -> Mutation means changing original data
+
+//Rule: If using react state, never mutate directly
+
+let arr = [1,2,3];
+arr.push(4)
+
+console.log(arr) //This is mutation and react hates it because it tracks state changes.
+
+//eg: 
+
+let todos = ["Study", "Start React"];
+
+let newTodos = [...todos, "Sleep"];
+console.log(todos);
+console.log(newTodos);
+
+
