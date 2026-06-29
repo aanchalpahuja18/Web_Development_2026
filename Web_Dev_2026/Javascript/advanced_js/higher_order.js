2f1aaa0b4e63820dad17c0d8fb353fcefc4a3840
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-06-29 15:06:39 +0530

    Higher Order Functions in JS

---
+++ Web_Dev_2026/Javascript/advanced_js/higher_order.js
@@ -0,0 +1,44 @@
+//Higher order functions: A Higher Order Function is a function that either:
+// 1. Takes another function as an argument
+// OR
+// 2. Returns another function
+
+//eg: map: here map is a higher order function since it takes a function as the argument and the inner function which is passed inside the map() is called as a callback function
+
+let arr = [1,2,3];
+
+let newArr = arr.map(function(el){
+    return el*el;
+})
+
+//map internally does the below:
+
+// for(each element){
+//    apply your function
+//    store returned value
+// }
+
+// eg: filter logic:
+// for(each element){
+//    if callback returns true
+//       keep it
+//    else
+//       remove it
+// }
+
+console.log(newArr);
+
+//JS says:
+
+// “Why repeat the loop?
+// I’ll handle looping internally.
+// You just tell me what operation to do.”
+
+// That is Higher Order Functions.
+
+// Difference:
+
+// map() → returns new array
+// filter() → returns filtered array
+// forEach() → returns nothing
+
